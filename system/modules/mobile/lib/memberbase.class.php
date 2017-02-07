0eb5eabc75cf8c375ad6ad2f71e60a15a6057c05
heizol.shao@gmail.com <heizol.shao@gmail.com>
2017-02-07 19:57:04 +0800

    user login

--- system/modules/mobile/lib/memberbase.class.php
+++ system/modules/mobile/lib/memberbase.class.php
@@ -6,34 +6,35 @@ System::load_sys_fun("user");
 class memberbase extends SystemAction {
 	protected $userinfo=NULL;	
 	public function __construct(){
+	    session_start();
 		$this->db = System::load_sys_class("model");
-		$uid=intval(_encrypt(_getcookie("uid"),'DECODE'));		
-		$ushell=_encrypt(_getcookie("ushell"),'DECODE');
+		$uid=$_SESSION['user_id'];		
+// 		$ushell=_encrypt(_getcookie("ushell"),'DECODE');
 		if(!$uid)$this->userinfo=false;
 		$this->userinfo=$this->db->GetOne("SELECT * from `@#_member` where `uid` = '$uid'");
 		if(!$this->userinfo)$this->userinfo=false;	
 	
-		$shell=md5($this->userinfo['uid'].$this->userinfo['password'].$this->userinfo['mobile'].$this->userinfo['email']);		
-		if($ushell!=$shell)$this->userinfo=false;
+// 		$shell=md5($this->userinfo['uid'].$this->userinfo['password'].$this->userinfo['mobile'].$this->userinfo['email']);		
+// 		if($ushell!=$shell)$this->userinfo=false;
 	}
 	
 	protected function checkuser($uid,$ushell){
-		$uid=intval(_encrypt($uid,'DECODE'));
-		$ushell=_encrypt($ushell,'DECODE');	
+		$uid=$_SESSION['user_id'];
+// 		$ushell=_encrypt($ushell,'DECODE');	
 		if(!$uid)return false;
-		if($ushell===NULL)return false;
+// 		if($ushell===NULL)return false;
 		$this->userinfo=$this->db->GetOne("SELECT * from `@#_member` where `uid` = '$uid'");
 		if(!$this->userinfo){
 			$this->userinfo=false;
 			return false;
 		}
-		$shell=md5($this->userinfo['uid'].$this->userinfo['password'].$this->userinfo['mobile'].$this->userinfo['email']);
-		if($ushell!=$shell){
-			$this->userinfo=false;
-			return false;
-		}else{
+// 		$shell=md5($this->userinfo['uid'].$this->userinfo['password'].$this->userinfo['mobile'].$this->userinfo['email']);
+// 		if($ushell!=$shell){
+// 			$this->userinfo=false;
+// 			return false;
+// 		}else{
 			return true;
-		}
+// 		}
 		
 	}
 	public function get_user_info(){
